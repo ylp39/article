406b544aa922b02965d2355e11a9cefdeb042f98
杨立培 <747136311@qq.com>
2019-07-17 08:11:41 +0000

    Model

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+    protected $table = 'articles';
+    protected $fillable = [
+        'title', 'content',
+    ];
 }
