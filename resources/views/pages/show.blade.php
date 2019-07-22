438871c2c9eff03eec34035a60a2bded6b49beca
杨立培 <747136311@qq.com>
2019-07-22 08:14:21 +0000

    宽度

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -14,7 +14,7 @@
             padding: 0;
             background-color: #fff;
             margin: 0 auto;
-            width: 60%;
+            width: 100%;
             height: 100%;
             text-align: center;
         }
